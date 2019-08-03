4c985db8e62ff01a1d42c62c290d4b7f2e0097ac
Fuadtag <Fuadjt@code.edu.az>
2019-08-03 17:31:52 +0400

    Intro done

--- js/main.js
+++ js/main.js
@@ -13,13 +13,14 @@ $(function() {
     let liabout = $(`<li class="nav-item"><a href = "#" class = "nav-link" >About</a></li>`);
     let liservices = $(`<li class="nav-item"><a href = "#" class = "nav-link" >Services</a></li>`);
     let licontact = $(`<li class="nav-item"><a href = "#" class = "nav-link" >Contact</a></li>`);
-    // let liabout = $("<li></li>");
-    // let liservices = $("<li></li>");
-    // let licontact = $("<li></li>");
-    // let ahome = $("<a></a>");
-    // let aabout = $("<a></a>");
-    // let aservices = $("<a></a>");
-    // let acontact = $("<a></a>");
+    let divintro = $(`<div class = "container jumbotron"></div>`);
+    $(divintro).css("margin-top", "100px")
+    let h1 = (`<h1 class = "display-3">A Warm Welcome!</h1>`);
+    let pelem = $(`<p class = "text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>`)
+    let button = $(`<button class = "btn btn-primary btn-lg mb-4">Call to action!</button>`);
+    let divrow = $(`<div class = "row text-center"></div>`);
+    let colrow1 = $(`<div class = "col-lg-3 col-md-6 mb-4"></div>`);
+    let divcard = $(`<div class = "card h-100"></div>`);
 
 
 
@@ -66,7 +67,9 @@ $(function() {
     $(abrand).addClass("navbar-brand");
     $("#divright").append(ul);
     $(ul).append(lihome, liabout, liservices, licontact);
-    $("a").css("")
+    $(main).append(divintro);
+    $(divintro).append(h1, pelem, button)
+
 
 
 
